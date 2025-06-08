c0542c3bc05ff21da37d16d0aaf745e16022b5c7
laukika-in <tech@laukika.com>
2025-06-08 14:17:28 +0530

    Update email-campaign.php

--- email-campaign.php
+++ email-campaign.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Email Campaign
 Description: Transactional email campaigns with scheduling and reporting.
-Version: 1.0.014
+Version: 1.0.015
 Author: ChatGPT
 License: GPLv2 or later
 */
